1e2c670f74a97294049d80c3b4d5071425a38e36
Mengzhen <gao.mengzhen94@gmail.com>
2017-02-16 15:08:17 -0500

    step2 - tobecontinued

---
+++ client/scripts/controllers/chats.controller.js
@@ -0,0 +1,9 @@
+import { Controller } from 'angular-ecmascript/module-helpers';
+
+export default class ChatsCtrl extends Controller {
+	constructor(){
+		super(...arguments);
+	}
+}
+
+ChatsCtrl.$name = 'ChatsCtrl';
